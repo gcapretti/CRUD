33794edd2fdc52ee03b2696aadee325db1f2846a
gcapretti <gaston6504@hotmail.com>
2020-06-02 13:00:03 -0200

    COMMIT 3

--- app/src/main/java/com/example/crud/MainActivity.java
+++ app/src/main/java/com/example/crud/MainActivity.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 //PROBANDO COMMIT2
+//asdadasdas
 //----------------------------------------INICIO----------------------------------------------------
 public class MainActivity extends AppCompatActivity {
 
