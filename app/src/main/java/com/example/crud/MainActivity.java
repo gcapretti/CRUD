70ddb7aeb9faa7122c5e4462802d9e67114fa82e
gcapretti <gaston6504@hotmail.com>
2020-06-02 21:12:25 -0200

    comentario

--- app/src/main/java/com/example/crud/MainActivity.java
+++ app/src/main/java/com/example/crud/MainActivity.java
@@ -26,6 +26,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+//COMENTARIO
 //----------------------------------------INICIO----------------------------------------------------
 public class MainActivity extends AppCompatActivity {
 
